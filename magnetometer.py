13ca5d2570681bfb7480113ead1f6df5f5447560
printable-labs <printable.labs.3d@gmail.com>
2026-08-20 12:04:44 +0100

    Update magnetometer.py

--- magnetometer.py
+++ magnetometer.py
@@ -15,7 +15,7 @@ class ADS1115_wDRDY:
     read() consumes queue and reads conversion result.
     """
 
-    def __init__(self, i2c_bus: int = 1, i2c_addr: int = 0x49, drdy_pin: int = 17, queue_size: int = 100, adc_resolution = "0_512"):
+    def __init__(self, i2c_bus: int = 1, i2c_addr: int = 0x49, drdy_pin: int = 6, queue_size: int = 100, adc_resolution = "0_512"):
         self.i2c_addr = i2c_addr
         self.bus = smbus2.SMBus(i2c_bus)
 
@@ -137,7 +137,7 @@ class ADS1115_wDRDY:
         return raw - 65536 if raw > 32767 else raw
 
 class MagnetometerReader:
-    def __init__(self,pi: pigpio.pi, drdy_pin=6,lis3mdl_addr = 0x1C, adc_addr = 0x49, max_data_points = 128, max_queue=256, magnetometer_resolution = "8", adc_resolution = "0_512"):
+    def __init__(self,pi: pigpio.pi, drdy_pin=25,lis3mdl_addr = 0x1C, adc_addr = 0x49, max_data_points = 128, max_queue=256, magnetometer_resolution = "8", adc_resolution = "0_512"):
         # Initialize sensor
         from board import I2C
         from adafruit_lis3mdl import LIS3MDL, Rate, Range
